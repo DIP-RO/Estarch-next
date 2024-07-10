104d46be80a250d1e638cc5e7bcf1c26565924ca
Dipro <dipropaul032@gamil.com>
2024-07-10 23:33:08 +0600

    added redux

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./navbar/page";
 import Footer from "./footer/page";
+import { Provider } from "react-redux";
+import store from "@/lib/store";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,9 +16,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        {children}
-        <Footer />
+        <Provider store={store}>
+          <NavBar />
+          {children}
+          <Footer />
+        </Provider>
       </body>
     </html>
   );
