519b79315ba99dcbfee0a4814f89117823442bd8
Dipro <dipropaul032@gamil.com>
2024-07-10 23:48:30 +0600

    added redux

---
+++ components/products/page.js
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "../lib/slices/productsSlice";
+
+export default function Products() {
+  const { products, isLoading, isError, error } = useSelector(state => state.products);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  let content;
+  if (isLoading) {
+    content = <h1>Loading....</h1>;
+  } else if (isError) {
+    content = <h1>{error}</h1>;
+  } else if (products.length === 0) {
+    content = <h1>No products found</h1>;
+  } else {
+    content = <ul>{products.map(product => <li key={product.id}>{product.title}</li>)}</ul>;
+  }
+
+  return <div>{content}</div>;
+}
