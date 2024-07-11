d2c32f899e0b17031519415d4ff4d953441a52d8
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-07-11 12:59:02 +0600

    update: Correction in category section

--- components/sellingCategory/SellingCategory.jsx
+++ components/sellingCategory/SellingCategory.jsx
@@ -13,6 +13,7 @@ import polo from '../../public/images/c_polo.jpeg'
 import boxer from '../../public/images/c_boxer.jpeg'
 
 const SellingCategory = () => {
+
     return (
         <div className='mx-6 md:mx-12 lg:mx-20'>
             <h1 className='text-center mt-12 font-bold md:text-2xl text-xl pb-5'>BROWSE OUR CATEGORY</h1>
@@ -51,37 +52,37 @@ const SellingCategory = () => {
                 <SwiperSlide>
                     <div className='relative'>
                         <Image className='rounded-md' width={500} src={shirt} alt="" />
-                        <button className='btn btn-xs md:btn-sm lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>SHIRT</button>
+                        <p className='cursor-pointer rounded-lg font-medium md:font-semibold text-xs md:text-sm py-1 bg-base-200 px-2 lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>SHIRT</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className='relative'>
                         <Image className='rounded-md' width={500} src={t_shirt} alt="" />
-                        <button className='btn btn-xs md:btn-sm lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>T-SHIRT</button>
+                        <p className='cursor-pointer rounded-lg font-medium md:font-semibold text-xs md:text-sm py-1 bg-base-200 px-2 lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>T-SHIRT</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className='relative'>
                         <Image className='rounded-md' width={500} src={panjabi} alt="" />
-                        <button className='btn btn-xs md:btn-sm lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>PANJABI</button>
+                        <p className='cursor-pointer rounded-lg font-medium md:font-semibold text-xs md:text-sm py-1 bg-base-200 px-2 lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>PANJABI</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className='relative'>
                         <Image className='rounded-md' width={500} src={pajama} alt="" />
-                        <button className='btn btn-xs md:btn-sm lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>PAJAMA</button>
+                        <p className='cursor-pointer rounded-lg font-medium md:font-semibold text-xs md:text-sm py-1 bg-base-200 px-2 lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>PAJAMA</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className='relative'>
                         <Image className='rounded-md' width={500} src={polo} alt="" />
-                        <button className='btn btn-xs md:btn-sm lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>POLO</button>
+                        <p className='cursor-pointer rounded-lg font-medium md:font-semibold text-xs md:text-sm py-1 bg-base-200 px-2 lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>POLO</p>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
                     <div className='relative'>
                         <Image className='rounded-md' width={500} src={boxer} alt="" />
-                        <button className='btn btn-xs md:btn-sm lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>BOXER</button>
+                        <p className='cursor-pointer rounded-lg font-medium md:font-semibold text-xs md:text-sm py-1 bg-base-200 px-2 lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>BOXER</p>
                     </div>
                 </SwiperSlide>
             </Swiper>
