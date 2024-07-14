24db72128a4c162727240f72892ac72b046a1598
tabrid <mollik15-5096@diu.edu.bd>
2024-07-15 01:30:44 +0600

    pos create

--- components/footer/page.js
+++ components/footer/page.js
@@ -143,6 +143,7 @@ export default function Footer() {
                 <Image
                   src={pic}
                   alt="Logo"
+                  layout="responsive"
                   width={48}
                   height={48}
                   className="rounded-full"
