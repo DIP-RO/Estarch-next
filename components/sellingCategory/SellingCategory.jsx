8ebdabd8e90080214024a0dd66a3f0b19407352f
Dipro <dipropaul032@gamil.com>
2024-08-07 13:33:03 +0600

    added

--- components/sellingCategory/SellingCategory.jsx
+++ components/sellingCategory/SellingCategory.jsx
@@ -1,31 +1,30 @@
-'use client'
+'use client';
 import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, FreeMode, Autoplay } from 'swiper/modules';
 import Image from 'next/image';
 
-import './styles.css'
-import shirt from '../../public/images/c_shirt.jpeg'
-import t_shirt from '../../public/images/c_t-shirt.jpeg'
-import panjabi from '../../public/images/c_panjabi.jpeg'
-import pajama from '../../public/images/c_pajama.jpeg'
-import polo from '../../public/images/c_polo.jpeg'
-import boxer from '../../public/images/c_boxer.jpeg'
+import './styles.css';
+import shirt from '../../public/images/c_shirt.jpeg';
+import t_shirt from '../../public/images/c_t-shirt.jpeg';
+import panjabi from '../../public/images/c_panjabi.jpeg';
+import pajama from '../../public/images/c_pajama.jpeg';
+import polo from '../../public/images/c_polo.jpeg';
+import boxer from '../../public/images/c_boxer.jpeg';
 import axios from 'axios';
 import Link from 'next/link';
 import baseUrl from '../services/baseUrl';
 
 const SellingCategory = () => {
-
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
         axios.get(`${baseUrl}/api/categories/categories`)
             .then(res => {
                 console.log(res.data);
-                setCategories(res.data)
-            })
-    }, [])
+                setCategories(res.data);
+            });
+    }, []);
 
     return (
         <div className='mx-0 md:mx-12 lg:mx-20'>
@@ -58,27 +57,22 @@ const SellingCategory = () => {
                         spaceBetween: 50,
                     },
                 }}
-
                 modules={[FreeMode, Navigation, Autoplay]}
                 className="mySwiper"
             >
-                {
-                    categories.map(cat =>
-                        <SwiperSlide>
-                            <Link href={`${cat.type.name}/${cat._id}`}>
-                                <div className='relative'>
-                                    <Image className='rounded-md' width={500}
-                                        height={0} src={cat.image} alt="" />
-                                    <p className='cursor-pointer rounded-lg font-medium md:font-semibold text-xs md:text-sm py-1 bg-base-200 px-2 lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20'>{cat.name}</p>
-                                    <div className="absolute top-0 bg-[#1111112f] z-10 w-full min-h-full">
-                                        {/* This div is use for a overlay on background image */}
-                                    </div>
-
+                {categories.map(cat => (
+                    <SwiperSlide key={cat._id}>
+                        <Link href={`${cat.type.name}/${cat._id}`}>
+                            <div className='relative'>
+                                <Image className='rounded-md' width={500} height={0} src={cat.image} alt="" />
+                                <p className='cursor-pointer rounded-lg font-medium md:font-semibold text-xs md:text-sm py-1 bg-base-200 px-2 lg:px-8 absolute bottom-1 md:bottom-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20'>{cat.name}</p>
+                                <div className="absolute top-0 bg-[#1111112f] z-10 w-full min-h-full">
+                                    {/* This div is use for a overlay on background image */}
                                 </div>
-                            </Link>
-                        </SwiperSlide>
-                    )
-                }
+                            </div>
+                        </Link>
+                    </SwiperSlide>
+                ))}
             </Swiper>
         </div>
     );
