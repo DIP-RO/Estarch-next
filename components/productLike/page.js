0c7e094777b159725418d8198326cf08d3a8a4b0
tabrid <mollik15-5096@diu.edu.bd>
2024-08-17 22:51:44 +0600

    added

--- components/productLike/page.js
+++ components/productLike/page.js
@@ -2,32 +2,58 @@
 import { useState } from "react";
 import Image from "next/image";
 import img from '../../public/images/c_boxer.jpeg';
+import { useDispatch } from "react-redux";
+import { openCardSlide } from "@/lib/slices/cardSlideSlice";
+import { addToCart } from "@/lib/slices/cartSlice";
 
 const ProductCard = (product) => {
   const [selectedSize, setSelectedSize] = useState("");
   console.log(product.product);
-  
+  const dispatch = useDispatch();
   const handleSizeChange = (e) => {
     setSelectedSize(e.target.value);
   };
-
+  const handleAddToCart = () => {
+      dispatch(openCardSlide())
+      const quantity = 1;
+      dispatch(addToCart({
+        id: product.product._id,
+        product: {
+          title: product.product.productName,
+          price: product.product?.salePrice,
+          colors: [{ images: [{ url: product.product.images[0] }] }],
+          stock: { quantity: 10 }, // Adjust based on actual product details
+        },
+        quantity,
+        color: 'Blue', // Add actual color if available
+        size: selectedSize,
+      }));
+  };
   return (
     <div className="flex flex-col md:flex-row mt-10 mx-4  p-4 border rounded-lg bg-white">
       <div className="flex flex-col md:flex-row md:items-center w-full">
         <Image
-          src={img} // replace with actual image path
+          src={product.product.images[0]}
           alt="10 inch Regular Fit Shorts"
-          className="object-cover rounded-md"
+          className="object-cover h-[150px] rounded-md"
           width={180}
           height={80}
         />
         <div className="ml-0 mt-4 md:mt-0 md:ml-4 flex-grow">
           <h3 className="text-gray-800 text-lg font-semibold">
-            10 inch Regular Fit Shorts
+            {product.product.productName}
           </h3>
-          <p className="text-gray-800 text-lg font-semibold">
-            199.00&nbsp;<span className="text-sm">AED</span>
-          </p>
+          {product.product?.regularPrice - product.product?.salePrice > 0 && (
+            <p className='my-1 text-[20px] text-black '>
+              <span className=''>TK.</span>{product.product?.salePrice}
+              <span className='md:text-[17px] line-through text-red-500'> Tk.{product.product?.regularPrice}</span>
+            </p>
+          )}
+          {product.product?.regularPrice - product.product?.salePrice <= 0 && (
+            <p className='my-1 text-[20px] text-black '>
+              <span className=''>TK.</span>{product.product?.salePrice}
+            </p>
+          )}
           <div className="mt-2 flex flex-col md:flex-row gap-4 items-center">
             <select
               className="border border-gray-300 rounded-md py-2 px-4 text-gray-700 w-full md:w-auto"
@@ -35,13 +61,14 @@ const ProductCard = (product) => {
               onChange={handleSizeChange}
             >
               <option value="">Select Size</option>
-              <option value="S">S</option>
-              <option value="M">M</option>
-              <option value="L">L</option>
-              <option value="XL">XL</option>
+              {product.product?.selectedSizes?.map((size) => (
+                <option key={size} value={size}>
+                  {size}
+                </option>
+              ))}
             </select>
             {selectedSize && (
-              <button className="w-full md:w-auto px-6 py-2 border border-gray-400 rounded-lg bg-blue-500 text-white hover:bg-blue-600 transition duration-300 ease-in-out">
+              <button onClick={handleAddToCart} className="w-full md:w-auto px-6 py-2 border border-gray-400 rounded-lg bg-blue-500 text-white hover:bg-blue-600 transition duration-300 ease-in-out">
                 ADD TO CART
               </button>
             )}
